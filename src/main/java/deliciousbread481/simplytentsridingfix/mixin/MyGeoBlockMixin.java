3bcd6793ddbd701460aba28e14b538b7e638020d
DeliciousBread481 <3574467868@qq.com>
2026-07-19 09:13:05 +0800

    Fix SRG issue

--- src/main/java/deliciousbread481/simplytentsridingfix/mixin/MyGeoBlockMixin.java
+++ src/main/java/deliciousbread481/simplytentsridingfix/mixin/MyGeoBlockMixin.java
@@ -5,15 +5,16 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(targets = "com.sappyeddie.simplytents.tent.block.MyGeoBlock")
+@Mixin(targets = "com.sappyeddie.simplytents.tent.block.MyGeoBlock", remap = false)
 public class MyGeoBlockMixin {
 
     @Redirect(
-        method = "use",
+        method = "m_6227_",
         at = @At(
             value = "INVOKE",
-            target = "Lnet/minecraft/world/entity/player/Player;isCrouching()Z"
-        )
+            target = "Lnet/minecraft/world/entity/player/Player;m_6047_()Z"
+        ),
+        remap = false
     )
     private boolean simplytentsridingfix$allowPackWhileRiding(Player player) {
         return player.isCrouching() || player.isPassenger();
